0154600733b734090c9e600975c4e7a7fda659f2
Oleksandr Kulynych <kulynych.olexandr@gmail.com>
2025-10-03 23:16:20 +0200

    feat: add deactivate expired links command

---
+++ app/app/Console/Commands/DeactivateExpiredLinks.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\AccessLink;
+use Illuminate\Console\Command;
+
+class DeactivateExpiredLinks extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:deactivate-expired-links';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Deactivate all expired links';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): int
+    {
+        $updatedRows = AccessLink::query()
+            ->where('is_active', true)
+            ->where('expires_at', '<=', now())
+            ->update(['is_active' => false]);
+
+        $this->info('Deactivated expired links: ' . $updatedRows);
+
+        return self::SUCCESS;
+    }
+}
